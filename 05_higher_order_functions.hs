356d1455cb586e2f61e875e6c5f1f2b006d9bbcf
doktorspaceman <beyondwrinkled@gmail.com>
2017-02-09 15:58:46 -0800

    working on higher order functions

---
+++ 05_higher_order_functions.hs
@@ -0,0 +1,15 @@
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred =  compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
