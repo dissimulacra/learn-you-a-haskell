a0d090d5514d0a9cbcfa14c24429e0f97c13d767
doktorspaceman <beyondwrinkled@gmail.com>
2017-02-27 08:52:13 -0800

    further work on modules

--- 06_modules.hs
+++ 06_modules.hs
@@ -1,5 +1,8 @@
 import Data.List
 import Data.Char
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Geometry
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -18,4 +21,12 @@ encode shift msg =
 decode :: Int -> String -> String
 decode shift msg = encode (negate shift) msg
 
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith (\number1 number2 -> number1 ++ ", " ++ number2) xs
 
