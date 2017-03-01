5eda7055b6d9bbcceae03ad1203a57f0766d14ad
doktorspaceman <beyondwrinkled@gmail.com>
2017-02-28 18:42:42 -0800

    making some progress on typeclasses

---
+++ 07_making_our_own_types_and_typeclasses.hs
@@ -0,0 +1,13 @@
+import Shapes
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
+
+data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
+
+
