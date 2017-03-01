7087e8a42e9290b1144d77444818fb2fb06d5fae
doktorspaceman <beyondwrinkled@gmail.com>
2017-03-01 09:32:50 -0800

    more progress on typeclasses

--- 07_making_our_own_types_and_typeclasses.hs
+++ 07_making_our_own_types_and_typeclasses.hs
@@ -10,4 +10,15 @@ data Person = Person { firstName :: String
 
 data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
 
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
 
