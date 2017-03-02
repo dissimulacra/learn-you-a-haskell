3bbdce22ce5038c13234f897eef57d8915bf1425
doktorspaceman <beyondwrinkled@gmail.com>
2017-03-01 18:42:02 -0800

    more progress on typeclasses

--- 07_making_our_own_types_and_typeclasses.hs
+++ 07_making_our_own_types_and_typeclasses.hs
@@ -1,12 +1,13 @@
 import Shapes
+import qualified Data.Map as Map
 
-data Person = Person { firstName :: String
-                     , lastName :: String
-                     , age :: Int
-                     , height :: Float
-                     , phoneNumber :: String
-                     , flavor :: String
-                     } deriving (Show)
+--data Person = Person { firstName :: String
+--                     , lastName :: String
+--                     , age :: Int
+--                     , height :: Float
+--                     , phoneNumber :: String
+--                     , flavor :: String
+--                     } deriving (Show)
 
 data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
 
@@ -21,4 +22,55 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
 scalarMult :: (Num t) => Vector t -> Vector t -> t
 (Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
 
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     } deriving (Eq, Show, Read)
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+--phoneBook :: [(String,String)]  
+--phoneBook =      
+--    [("betty","555-2938")     
+--    ,("bonnie","452-2928")     
+--    ,("patsy","493-2928")     
+--    ,("lucille","205-2928")     
+--    ,("wendy","939-8282")     
+--    ,("penny","853-2492")     
+--    ]  
+
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name,PhoneNumber)]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook
+
+type AssocList k v = [(k,v)]
+
+type IntMap = Map.Map Int
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
+        Just (state, code) -> if state /= Taken
+                                then Right code
+                                else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+lockers :: LockerMap
+lockers = Map.fromList
+    [(100,(Taken,"ZD39I"))  
+    ,(101,(Free,"JAH3I"))  
+    ,(103,(Free,"IQSA9"))  
+    ,(105,(Free,"QOTSA"))  
+    ,(109,(Taken,"893JJ"))  
+    ,(110,(Taken,"99292"))  
+    ] 
 
