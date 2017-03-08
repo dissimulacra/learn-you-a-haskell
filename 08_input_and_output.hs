4fc6e692db08ed3f8a262f756565f1fd406c7a00
doktorspaceman <beyondwrinkled@gmail.com>
2017-03-08 08:43:48 -0800

    further io progress

--- 08_input_and_output.hs
+++ 08_input_and_output.hs
@@ -109,13 +109,33 @@ import Control.Monad
 
 import System.IO
 
-main = do
-    handle <- openFile "girlfriend.txt" ReadMode
-    contents <- hGetContents handle
-    putStr contents
+--main = do
+--    handle <- openFile "girlfriend.txt" ReadMode
+--    contents <- hGetContents handle
+--    putStr contents
+--    hClose handle
+
+--main = do
+--    --withFile "girlfriend.txt" ReadMode (\handle -> do
+--    --    contents <- hGetContents handle
+--    --    putStr contents)
+--    contents <- readFile "girlfriend.txt"
+--    putStr contents
+
+withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
+withFile' path mode f = do
+    handle <- openFile path mode
+    result <- f handle
     hClose handle
+    return result
 
+--main = do
+--    contents <- readFile "girlfriend.txt"
+--    writeFile "girlfriendcaps.txt" (map toUpper contents)
 
+main = do
+    todoItem <- getLine
+    appendFile "todo.txt" (todoItem ++ "\n")
 
 
 
