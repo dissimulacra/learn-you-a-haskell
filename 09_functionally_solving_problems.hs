ad38b960935efcaee21e6d629100649e274f494f
doktorspaceman <beyondwrinkled@gmail.com>
2017-03-09 18:38:50 -0800

    further functional solving

--- 09_functionally_solving_problems.hs
+++ 09_functionally_solving_problems.hs
@@ -1,5 +1,8 @@
+import Data.List
 
-
-solveRPN :: (Num a) => String -> a
-solveRPN expression = head (foldl foldingFunction [] (words expression))
-    where foldingFunction stack item = ...
+solveRPN :: (Num a, Read a) => String -> a
+solveRPN = head . foldl foldingFunction [] . words
+    where  foldingFunction (x:y:ys) "*" = (x * y):ys
+           foldingFunction (x:y:ys) "+" = (x + y):ys
+           foldingFunction (x:y:ys) "-" = (y - x):ys
+           foldingFunction xs numberString = read numberString:xs
